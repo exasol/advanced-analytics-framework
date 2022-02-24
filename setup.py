fea50ababddbf8f04ecf539251d00e095fb252df
Umit Cavus Buyuksahin <ucbuyuksahin@gmail.com>
2022-02-24 12:43:44 +0100

    #1:Added initial setup files (#2)
    * Added initial setup files
    
    * Updated reviews

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['exasol_advanced_analytics_framework']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'exasol-advanced-analytics-framework',
+    'version': '0.1.0',
+    'description': 'Framework for building complex data analysis algorithms with Exasol',
+    'long_description': '',
+    'author': 'Umit Buyuksahin',
+    'author_email': 'umit.buyuksahin@exasol.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/exasol/advanced-analytics-framework',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ tests/test_advanced_analytics_framework.py
@@ -0,0 +1,5 @@
+from exasol_advanced_analytics_framework import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
