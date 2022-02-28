1d9a63400e235b28931780b793ee633deb3a5c7d
Umit Cavus Buyuksahin <ucbuyuksahin@gmail.com>
2022-02-28 14:16:10 +0100

    #9: Added README file (#10)
    * Added readme file
    
    * Updated the setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup_kwargs = {
     'name': 'exasol-advanced-analytics-framework',
     'version': '0.1.0',
     'description': 'Framework for building complex data analysis algorithms with Exasol',
-    'long_description': '',
+    'long_description': '# Exasol Advanced Analytics Framework\n\n**This project is at an early development stage.**\n\nFramework for building complex data analysis algorithms with Exasol.\n\n\n## Information for Users\n\n- [User Guide](doc/user_guide/user_guide.md)\n- [System Requirements](doc/system_requirements.md)\n- [Design](doc/design.md)\n- [License](LICENSE)\n\n## Information for Developers\n\n- [Developers Guide](doc/developer_guide/developer_guide.md)\n\n',
     'author': 'Umit Buyuksahin',
     'author_email': 'umit.buyuksahin@exasol.com',
     'maintainer': None,
