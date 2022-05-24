57f49a52ec4edf44dca29d789c810e71810cd4c7
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-05-24 13:49:02 +0200

    Added integration tests

--- tests/test_package/test_event_handlers/event_handler_test.py
+++ tests/test_package/test_event_handlers/event_handler_test.py
@@ -8,10 +8,13 @@ from exasol_advanced_analytics_framework.event_handler.event_handler_result \
     import EventHandlerResultBase, EventHandlerResultFinished
 
 
+FINAL_RESULT = {"result": 1}
+
+
 class EventHandlerTest(EventHandlerBase):
     def handle_event(self,
                      exa_context: EventContextBase,
                      event_handler_context: EventHandlerContext) -> \
             EventHandlerResultBase:
-        return EventHandlerResultFinished(final_result={"result": 1})
+        return EventHandlerResultFinished(final_result=FINAL_RESULT)
 
