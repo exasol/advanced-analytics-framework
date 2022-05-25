68194076c0c04e5973cefb93bbfea6884ff4473e
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-05-25 08:55:06 +0200

    Update toml file

--- exasol_advanced_analytics_framework/event_handler/event_handler_state.py
+++ exasol_advanced_analytics_framework/event_handler/event_handler_state.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import List
-
-from exasol_udf_mock_python.column import Column
+from exasol_data_science_utils_python.preprocessing.sql.schema.column import \
+    Column
 
 from exasol_advanced_analytics_framework.event_handler.event_handler_base \
     import EventHandlerBase
