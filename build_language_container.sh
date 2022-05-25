28f625344ea57e2a5c9b3bf17a104c6ff25cf19e
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-05-25 11:37:51 +0200

    Added release option

--- build_language_container.sh
+++ build_language_container.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -eo pipefail
+
+# set release goal, set to test by default
+RELEASE_GOAL="--release-goal test"
+if [ ! -z $1 ]
+then
+  if [ "$1" = "release" ]
+  then
+    RELEASE_GOAL=""
+  else
+    echo "Invalid release goal: $1"
+    exit
+  fi
+fi
+
 
 
 # main package - release
@@ -18,23 +32,26 @@ cp dist/*.whl "$RELEASE_BUILD_STEP_DIST_DIRECTORY"
 
 
 # test package - test
-TEST_PACKAGE_SCRIPT_DIR="$SCRIPT_DIR/tests/test_package"
-pushd  $TEST_PACKAGE_SCRIPT_DIR &> /dev/null
-poetry build
+if [ ! "$1" = "release" ]
+then
+  TEST_PACKAGE_SCRIPT_DIR="$SCRIPT_DIR/tests/test_package"
+  pushd  $TEST_PACKAGE_SCRIPT_DIR &> /dev/null
+  poetry build
 
-RELEASE_BUILD_STEP_DIST_DIRECTORY="$SCRIPT_DIR/$FLAVOR_PATH/flavor_base/test/dist"
-echo "Copy" dist/*.whl "$RELEASE_BUILD_STEP_DIST_DIRECTORY"
-mkdir -p "$RELEASE_BUILD_STEP_DIST_DIRECTORY" || true
-cp dist/*.whl "$RELEASE_BUILD_STEP_DIST_DIRECTORY"
+  RELEASE_BUILD_STEP_DIST_DIRECTORY="$SCRIPT_DIR/$FLAVOR_PATH/flavor_base/test/dist"
+  echo "Copy" dist/*.whl "$RELEASE_BUILD_STEP_DIST_DIRECTORY"
+  mkdir -p "$RELEASE_BUILD_STEP_DIST_DIRECTORY" || true
+  cp dist/*.whl "$RELEASE_BUILD_STEP_DIST_DIRECTORY"
 
 
-# switch to main directory
-popd
+  # switch to main directory
+  popd
+fi
 
 
 # build container
 echo "Build container"
-./language_container/exaslct export --flavor-path "$FLAVOR_PATH" --release-goal test
+./language_container/exaslct export --flavor-path "$FLAVOR_PATH" $RELEASE_GOAL
 
 echo "Generate language activation"
 ./language_container/exaslct generate-language-activation --flavor-path "$FLAVOR_PATH" --bucketfs-name bfsdefault --bucket-name default --path-in-bucket container --container-name "$FLAVOR_NAME"
