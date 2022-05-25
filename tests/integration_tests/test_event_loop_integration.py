e0d7bf7341775d06586dc5ef3f3f87ae099e28d0
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-05-25 08:55:22 +0200

    Added two iteration tests

--- tests/integration_tests/test_event_loop_integration.py
+++ tests/integration_tests/test_event_loop_integration.py
@@ -1,65 +1,18 @@
 import json
 import textwrap
-from click.testing import CliRunner
-from exasol_advanced_analytics_framework import deploy
 from tests.test_package.test_event_handlers.event_handler_test import \
     FINAL_RESULT
-from tests.utils.parameters import bucketfs_params, db_params
 
 
-def _create_schema(db_conn, schema):
-    db_conn.execute(f"DROP SCHEMA IF EXISTS {schema} CASCADE;")
-    db_conn.execute(f"CREATE SCHEMA IF NOT EXISTS {schema};")
+def test_event_loop_integration_with_one_iteration(
+        pyexasol_connection, upload_language_container, setup_database):
 
-
-def _deploy_scripts(language_alias, schema_name):
-    args_list = [
-        "scripts",
-        "--dsn", db_params.address(),
-        "--user", db_params.user,
-        "--pass", db_params.password,
-        "--schema", schema_name,
-        "--language-alias", language_alias,
-        "--develop"
-    ]
-    runner = CliRunner()
-    result = runner.invoke(deploy.main, args_list)
-    assert result.exit_code == 0
-
-
-def _create_bucketfs_connection(db_conn, bucketfs_connection_name):
-    query = "CREATE OR REPLACE  CONNECTION {name} TO '{uri}' " \
-            "USER '{user}' IDENTIFIED BY '{pwd}'".format(
-        name=bucketfs_connection_name,
-        uri=bucketfs_params.address(),
-        user=bucketfs_params.user,
-        pwd=bucketfs_params.password)
-    db_conn.execute(query)
-
-
-def setup_database(language_alias, schema, db_conn,
-                   bucketfs_connection_name):
-    _create_schema(db_conn, schema)
-    _deploy_scripts(language_alias, schema)
-    _create_bucketfs_connection(db_conn, bucketfs_connection_name)
-
-
-def test_event_loop_integration(request, pyexasol_connection,
-                                upload_language_container):
-    schema_name = request.node.name
-    bucketfs_connection_name = "TEST_AAF_BFS_CONN"
-    language_alias = "PYTHON3_AAF"
-    setup_database(
-        language_alias=language_alias,
-        schema=schema_name,
-        db_conn=pyexasol_connection,
-        bucketfs_connection_name=bucketfs_connection_name)
-
+    bucketfs_connection_name, schema_name = setup_database
     args = json.dumps(
         {"schema": f"{schema_name}",
          "bucketfs_connection": f"{bucketfs_connection_name}",
          "event_handler_module": "test_event_handlers.event_handler_test",
-         "event_handler_class": "EventHandlerTest",
+         "event_handler_class": "EventHandlerTestWithOneIteration",
          "event_handler_parameters": "{}"})
 
     query = f"EXECUTE SCRIPT {schema_name}.AAF_EVENT_LOOP('{args}')"
@@ -68,3 +21,18 @@ def test_event_loop_integration(request, pyexasol_connection,
     assert result[0][0] == str(FINAL_RESULT)
 
 
+def test_event_loop_integration_with_two_iteration(
+        setup_database, pyexasol_connection, upload_language_container):
+
+    bucketfs_connection_name, schema_name = setup_database
+    args = json.dumps(
+        {"schema": f"{schema_name}",
+         "bucketfs_connection": f"{bucketfs_connection_name}",
+         "event_handler_module": "test_event_handlers.event_handler_test",
+         "event_handler_class": "EventHandlerTestWithOneIteration",
+         "event_handler_parameters": "{}"})
+
+    query = f"EXECUTE SCRIPT {schema_name}.AAF_EVENT_LOOP('{args}')"
+    result = pyexasol_connection.execute(textwrap.dedent(query)).fetchall()
+
+    assert result[0][0] == str(FINAL_RESULT)
--- tests/test_package/test_event_handlers/event_handler_test.py
+++ tests/test_package/test_event_handlers/event_handler_test.py
@@ -1,3 +1,10 @@
+from typing import Dict, Any
+from exasol_data_science_utils_python.preprocessing.sql.schema.column import \
+    Column
+from exasol_data_science_utils_python.preprocessing.sql.schema.column_name \
+    import ColumnName
+from exasol_data_science_utils_python.preprocessing.sql.schema.column_type \
+    import ColumnType
 from exasol_advanced_analytics_framework.event_context.event_context_base \
     import EventContextBase
 from exasol_advanced_analytics_framework.event_handler.event_handler_base \
@@ -5,16 +12,44 @@ from exasol_advanced_analytics_framework.event_handler.event_handler_base \
 from exasol_advanced_analytics_framework.event_handler.event_handler_context \
     import EventHandlerContext
 from exasol_advanced_analytics_framework.event_handler.event_handler_result \
-    import EventHandlerResultBase, EventHandlerResultFinished
-
+    import EventHandlerResultBase, EventHandlerResultFinished, \
+    EventHandlerReturnQuery, EventHandlerResultContinue
 
 FINAL_RESULT = {"result": 1}
+QUERY_LIST = ["SELECT 1 FROM DUAL", "SELECT 2 FROM DUAL"]
 
 
-class EventHandlerTest(EventHandlerBase):
+class EventHandlerTestWithOneIteration(EventHandlerBase):
     def handle_event(self,
                      exa_context: EventContextBase,
                      event_handler_context: EventHandlerContext) -> \
             EventHandlerResultBase:
         return EventHandlerResultFinished(final_result=FINAL_RESULT)
 
+
+class EventHandlerTestWithTwoIteration(EventHandlerBase):
+    def __init__(self, parameters: Dict[str, Any]):
+        super().__init__(parameters)
+        self.iter = 0
+
+    def handle_event(self,
+                     exa_context: EventContextBase,
+                     event_handler_context: EventHandlerContext) -> \
+            EventHandlerResultBase:
+
+        if self.iter > 0:
+            event_handler_result = EventHandlerResultFinished(
+                final_result=FINAL_RESULT)
+        else:
+            return_query = "SELECT 1 AS COL1, 2 AS COL2 FROM DUAL"
+            return_query_columns = [
+                Column(ColumnName("COL1"), ColumnType("INTEGER")),
+                Column(ColumnName("COL2"), ColumnType("INTEGER"))]
+            event_handler_return_query = EventHandlerReturnQuery(
+                query=return_query,
+                query_columns=return_query_columns)
+            event_handler_result = EventHandlerResultContinue(
+                query_list=QUERY_LIST,
+                return_query=event_handler_return_query)
+        self.iter += 1
+        return event_handler_result
