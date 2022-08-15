1c65045e1d4202c71df08621cfc59f25ef685259
Torsten Kilias <torsten.kilias@exasol.com>
2022-08-15 11:53:18 +0200

    Fix UDFEventContext creation for first iteration

--- exasol_advanced_analytics_framework/event_context/udf_event_context.py
+++ exasol_advanced_analytics_framework/event_context/udf_event_context.py
@@ -19,6 +19,7 @@ class UDFEventContext(EventContext):
         self._start_col = start_col
         self._ctx = ctx
         self._has_next = True
+        print("column_mapping", column_mapping)
         self._reverse_column_mapping = \
             collections.OrderedDict(
                 [(value, key) for key, value in column_mapping.items()])
