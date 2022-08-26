82562d5b88626f85b228a963d7c16becd37c603a
Torsten Kilias <torsten.kilias@exasol.com>
2022-08-26 15:46:30 +0200

    Rename run_in_conda.sh into run_in_dev_env.sh

--- scripts/install_luarocks_packages.sh
+++ scripts/install_luarocks_packages.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-$SCRIPT_DIR/run_in_conda.sh luarocks --tree .luarocks install advanced-analytics-framework-*.rockspec
+$SCRIPT_DIR/run_in_dev_env.sh luarocks --tree .luarocks install advanced-analytics-framework-*.rockspec
