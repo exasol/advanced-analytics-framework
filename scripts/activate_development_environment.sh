efd84f8bd0be1297dc12674e138e2f0845a464c3
Torsten Kilias <torsten.kilias@exasol.com>
2022-08-26 15:14:48 +0200

    Refactor dev environment activation and adapted Github Workflows acordingly

--- scripts/activate_development_environment.sh
+++ scripts/activate_development_environment.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+micromamba activate --prefix ./.conda_env
 readonly export_lua_path_file=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 luarocks_tree_path="$export_lua_path_file"/../.luarocks
 eval "$(luarocks --tree "$luarocks_tree_path"  path)"
 export PATH="$luarocks_tree_path/bin:$PATH"
+poetry env use python3.8
--- scripts/run_in_conda.sh
+++ scripts/run_in_conda.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -i
 
-micromamba run --prefix ./.conda_env "$@"
+SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+source "$SCRIPT_DIR"/activate_development_environment.sh
+
+echo "Running following command inside the development environment:" "${@}"
+
+"${@}"
