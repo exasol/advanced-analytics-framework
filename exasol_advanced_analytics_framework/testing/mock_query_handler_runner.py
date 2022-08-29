05f6827785f20f04dea5ee68227eedb8e51725d6
Torsten Kilias <torsten.kilias@exasol.com>
2022-08-29 08:31:24 +0200

    Add MockQueryHandlerRunner for testing QueryHandler

---
+++ exasol_advanced_analytics_framework/testing/mock_query_handler_runner.py
@@ -0,0 +1,100 @@
+from typing import Callable, TypeVar, Generic, Tuple
+
+from exasol_data_science_utils_python.udf_utils.sql_executor import SQLExecutor
+
+from exasol_advanced_analytics_framework.query_handler.context.scope_query_handler_context import \
+    ScopeQueryHandlerContext
+from exasol_advanced_analytics_framework.query_handler.context.top_level_query_handler_context import \
+    TopLevelQueryHandlerContext
+from exasol_advanced_analytics_framework.query_handler.query.select_query import SelectQueryWithColumnDefinition
+from exasol_advanced_analytics_framework.query_handler.query_handler import QueryHandler
+from exasol_advanced_analytics_framework.query_handler.result import Continue, Finish, Result
+from exasol_advanced_analytics_framework.query_result.mock_query_result import MockQueryResult
+from exasol_advanced_analytics_framework.udf_framework.query_handler_runner_state import QueryHandlerRunnerState
+
+ResultType = TypeVar("ResultType")
+ParameterType = TypeVar("ParameterType")
+
+
+class MockQueryHandlerRunner(Generic[ParameterType, ResultType]):
+
+    def __init__(self,
+                 sql_executor: SQLExecutor,
+                 top_level_query_handler_context: TopLevelQueryHandlerContext,
+                 parameter: ParameterType,
+                 query_handler_factory: Callable[
+                     [ParameterType, ScopeQueryHandlerContext],
+                     QueryHandler[ParameterType, ResultType]]):
+        self._sql_executor = sql_executor
+        query_handler = query_handler_factory(parameter, top_level_query_handler_context)
+        self._state = QueryHandlerRunnerState(
+            top_level_query_handler_context=top_level_query_handler_context,
+            query_handler=query_handler
+        )
+
+    def run(self) -> ResultType:
+        try:
+            result = self._state.query_handler.start()
+            if isinstance(result, Finish):
+                self.handle_finish()
+                return result.result
+            else:
+                while isinstance(result, Continue):
+                    result = self.handle_continue(result)
+                if isinstance(result, Finish):
+                    self.handle_finish()
+                    return result.result
+                else:
+                    raise RuntimeError("Unknown Result")
+        except Exception as e:
+            self.handle_finish()
+            raise RuntimeError(f"Execution of query handler {self._state.query_handler} failed.") from e
+
+    def handle_continue(self, result: Continue):
+        self.release_and_create_new_return_query_query_handler_context()
+        self.cleanup_query_handler_context()
+        for query in result.query_list:
+            self._sql_executor.execute(query.query_string)
+        input_query_result = self.run_input_query(result)
+        result = self._state.query_handler.handle_query_result(input_query_result)
+        return result
+
+    def run_input_query(self, result: Continue):
+        input_query_view, input_query = self._wrap_return_query(result.input_query)
+        self._sql_executor.execute(input_query_view)
+        input_query_result_set = self._sql_executor.execute(input_query)
+        # TODO compare result_set columns with result.input_query.output_columns
+        input_query_result_table = input_query_result_set.fetchall()
+        input_query_result = MockQueryResult(data=input_query_result_table,
+                                             columns=result.input_query.output_columns)
+        return input_query_result
+
+    def handle_finish(self):
+        if self._state.input_query_query_handler_context is not None:
+            self._state.input_query_query_handler_context.release()
+        self._state.top_level_query_handler_context.release()
+        self.cleanup_query_handler_context()
+
+    def cleanup_query_handler_context(self):
+        cleanup_query_list = \
+            self._state.top_level_query_handler_context.cleanup_released_object_proxies()
+        for query in cleanup_query_list:
+            self._sql_executor.execute(query.query_string)
+
+    def release_and_create_new_return_query_query_handler_context(self):
+        if self._state.input_query_query_handler_context is not None:
+            self._state.input_query_query_handler_context.release()
+        self._state.input_query_query_handler_context = \
+            self._state.top_level_query_handler_context.get_child_query_handler_context()
+
+    def _wrap_return_query(self, input_query: SelectQueryWithColumnDefinition) -> Tuple[str, str]:
+        temporary_view = self._state.input_query_query_handler_context.get_temporary_view()
+        input_query_create_view_string = \
+            f"CREATE VIEW {temporary_view.name().fully_qualified()} AS {input_query.query_string};"
+        full_qualified_columns = [col.name.fully_qualified()
+                                  for col in input_query.output_columns]
+        columns_str = ",".join(full_qualified_columns)
+        input_query_string = \
+            f"SELECT {columns_str} " \
+            f"FROM {temporary_view.name().fully_qualified()};"
+        return input_query_create_view_string, input_query_string
---
+++ tests/unit_tests/query_handler_runner/test_mock_query_handler_runner.py
@@ -0,0 +1,380 @@
+from pathlib import PurePosixPath
+from typing import Union
+
+import pytest
+from exasol_bucketfs_utils_python.localfs_mock_bucketfs_location import LocalFSMockBucketFSLocation
+from exasol_data_science_utils_python.preprocessing.sql.schema.column import Column
+from exasol_data_science_utils_python.preprocessing.sql.schema.column_name import ColumnName
+from exasol_data_science_utils_python.preprocessing.sql.schema.column_type import ColumnType
+
+from exasol_advanced_analytics_framework.query_handler.context.scope_query_handler_context import \
+    ScopeQueryHandlerContext
+from exasol_advanced_analytics_framework.query_handler.context.top_level_query_handler_context import \
+    TopLevelQueryHandlerContext
+from exasol_advanced_analytics_framework.query_handler.query.select_query import SelectQueryWithColumnDefinition, \
+    SelectQuery
+from exasol_advanced_analytics_framework.query_handler.query_handler import QueryHandler
+from exasol_advanced_analytics_framework.query_handler.result import Continue, Finish
+from exasol_advanced_analytics_framework.query_result.query_result import QueryResult
+from exasol_advanced_analytics_framework.testing.mock_query_handler_runner import MockQueryHandlerRunner
+from exasol_advanced_analytics_framework.testing.mock_result_set import MockResultSet
+from exasol_advanced_analytics_framework.testing.mock_sql_executor import MockSQLExecutor
+
+
+@pytest.fixture()
+def temporary_schema_name():
+    temporary_schema_name = "temp_schema_name"
+    return temporary_schema_name
+
+
+@pytest.fixture()
+def top_level_query_handler_context(tmp_path, temporary_schema_name):
+    top_level_query_handler_context = TopLevelQueryHandlerContext(
+        temporary_bucketfs_location=LocalFSMockBucketFSLocation(base_path=PurePosixPath(tmp_path) / "bucketfs"),
+        temporary_db_object_name_prefix="temp_db_object",
+        temporary_schema_name=temporary_schema_name,
+    )
+    return top_level_query_handler_context
+
+
+class TestInput:
+    pass
+
+
+class TestOutput:
+    def __init__(self, test_input: TestInput):
+        self.test_input = test_input
+
+
+class StartFinishTestQueryHandler(QueryHandler[TestInput, TestOutput]):
+    def __init__(self, parameter: TestInput, query_handler_context: ScopeQueryHandlerContext):
+        super().__init__(parameter, query_handler_context)
+        self._parameter = parameter
+
+    def start(self) -> Union[Continue, Finish[TestOutput]]:
+        return Finish[TestOutput](TestOutput(self._parameter))
+
+    def handle_query_result(self, query_result: QueryResult) -> Union[Continue, Finish[TestOutput]]:
+        pass
+
+
+def test_start_finish(top_level_query_handler_context):
+    sql_executor = MockSQLExecutor(result_sets=[])
+    test_input = TestInput()
+    query_handler_runner = MockQueryHandlerRunner[TestInput, TestOutput](
+        sql_executor=sql_executor,
+        top_level_query_handler_context=top_level_query_handler_context,
+        parameter=test_input,
+        query_handler_factory=StartFinishTestQueryHandler
+    )
+    test_output = query_handler_runner.run()
+    assert test_output.test_input == test_input
+
+
+class StartFinishCleanupQueriesTestQueryHandler(QueryHandler[TestInput, TestOutput]):
+    def __init__(self, parameter: TestInput, query_handler_context: ScopeQueryHandlerContext):
+        super().__init__(parameter, query_handler_context)
+        self._parameter = parameter
+
+    def start(self) -> Union[Continue, Finish[TestOutput]]:
+        self._query_handler_context.get_temporary_table()
+        return Finish[TestOutput](TestOutput(self._parameter))
+
+    def handle_query_result(self, query_result: QueryResult) -> Union[Continue, Finish[TestOutput]]:
+        pass
+
+
+def test_start_finish_cleanup_queries(temporary_schema_name, top_level_query_handler_context):
+    sql_executor = MockSQLExecutor(result_sets=[MockResultSet()])
+    test_input = TestInput()
+    query_handler_runner = MockQueryHandlerRunner[TestInput, TestOutput](
+        sql_executor=sql_executor,
+        top_level_query_handler_context=top_level_query_handler_context,
+        parameter=test_input,
+        query_handler_factory=StartFinishCleanupQueriesTestQueryHandler
+    )
+    test_output = query_handler_runner.run()
+    assert test_output.test_input == test_input and \
+           sql_executor.queries == [f"""DROP TABLE IF EXISTS "{temporary_schema_name}"."temp_db_object_1";"""]
+
+
+class StartErrorCleanupQueriesTestQueryHandler(QueryHandler[TestInput, TestOutput]):
+    def __init__(self, parameter: TestInput, query_handler_context: ScopeQueryHandlerContext):
+        super().__init__(parameter, query_handler_context)
+        self._parameter = parameter
+
+    def start(self) -> Union[Continue, Finish[TestOutput]]:
+        self._query_handler_context.get_temporary_table()
+        raise Exception("Start failed")
+
+    def handle_query_result(self, query_result: QueryResult) -> Union[Continue, Finish[TestOutput]]:
+        pass
+
+
+def test_start_error_cleanup_queries(temporary_schema_name, top_level_query_handler_context):
+    sql_executor = MockSQLExecutor(result_sets=[MockResultSet()])
+    test_input = TestInput()
+    query_handler_runner = MockQueryHandlerRunner[TestInput, TestOutput](
+        sql_executor=sql_executor,
+        top_level_query_handler_context=top_level_query_handler_context,
+        parameter=test_input,
+        query_handler_factory=StartErrorCleanupQueriesTestQueryHandler
+    )
+    with pytest.raises(Exception, match="Execution of query handler .* failed."):
+        test_output = query_handler_runner.run()
+    assert sql_executor.queries == [f"""DROP TABLE IF EXISTS "{temporary_schema_name}"."temp_db_object_1";"""]
+
+
+class ContinueFinishTestQueryHandler(QueryHandler[TestInput, TestOutput]):
+    def __init__(self, parameter: TestInput, query_handler_context: ScopeQueryHandlerContext):
+        super().__init__(parameter, query_handler_context)
+        self._parameter = parameter
+
+    def start(self) -> Union[Continue, Finish[TestOutput]]:
+        column_name = ColumnName("a")
+        input_query = SelectQueryWithColumnDefinition(f"""SELECT 1 as {column_name.quoted_name()}""",
+                                                      [Column(column_name, ColumnType("INTEGER"))])
+        return Continue(query_list=[], input_query=input_query)
+
+    def handle_query_result(self, query_result: QueryResult) -> Union[Continue, Finish[TestOutput]]:
+        if query_result.a != 1:
+            raise AssertionError(f"query_result.a != 1, got {query_result.a}")
+        return Finish[TestOutput](TestOutput(self._parameter))
+
+
+def test_continue_finish(temporary_schema_name, top_level_query_handler_context):
+    input_query_create_view_result_set = MockResultSet()
+    input_query_result_set = MockResultSet(rows=[(1,)], columns={"a": {"type": "INTEGER"}})
+    drop_input_query_view_result_set = MockResultSet()
+    sql_executor = MockSQLExecutor(
+        result_sets=[
+            input_query_create_view_result_set,
+            input_query_result_set,
+            drop_input_query_view_result_set
+        ])
+    test_input = TestInput()
+    query_handler_runner = MockQueryHandlerRunner[TestInput, TestOutput](
+        sql_executor=sql_executor,
+        top_level_query_handler_context=top_level_query_handler_context,
+        parameter=test_input,
+        query_handler_factory=ContinueFinishTestQueryHandler
+    )
+    test_output = query_handler_runner.run()
+    assert test_output.test_input == test_input and \
+           sql_executor.queries == [
+               f"""CREATE VIEW "{temporary_schema_name}"."temp_db_object_2_1" AS SELECT 1 as "a";""",
+               f"""SELECT "a" FROM "{temporary_schema_name}"."temp_db_object_2_1";""",
+               f"""DROP VIEW IF EXISTS "{temporary_schema_name}"."temp_db_object_2_1";""",
+           ]
+
+
+class ContinueQueryListTestQueryHandler(QueryHandler[TestInput, TestOutput]):
+    def __init__(self, parameter: TestInput, query_handler_context: ScopeQueryHandlerContext):
+        super().__init__(parameter, query_handler_context)
+        self._parameter = parameter
+
+    def start(self) -> Union[Continue, Finish[TestOutput]]:
+        column_name = ColumnName("a")
+        input_query = SelectQueryWithColumnDefinition(f"""SELECT 1 as {column_name.quoted_name()}""",
+                                                      [Column(column_name, ColumnType("INTEGER"))])
+        query_list = [SelectQuery(query_string="SELECT 1")]
+        return Continue(query_list=query_list, input_query=input_query)
+
+    def handle_query_result(self, query_result: QueryResult) -> Union[Continue, Finish[TestOutput]]:
+        if query_result.a != 1:
+            raise AssertionError(f"query_result.a != 1, got {query_result.a}")
+        return Finish[TestOutput](TestOutput(self._parameter))
+
+
+def test_continue_query_list(temporary_schema_name, top_level_query_handler_context):
+    input_query_create_view_result_set = MockResultSet()
+    input_query_result_set = MockResultSet(rows=[(1,)], columns={"a": {"type": "INTEGER"}})
+    drop_input_query_view_result_set = MockResultSet()
+    query_list_result_set = MockResultSet(rows=[(1,)], columns={"a": {"type": "INTEGER"}})
+    sql_executor = MockSQLExecutor(
+        result_sets=[
+            input_query_create_view_result_set,
+            input_query_result_set,
+            query_list_result_set,
+            drop_input_query_view_result_set
+        ])
+    test_input = TestInput()
+    query_handler_runner = MockQueryHandlerRunner[TestInput, TestOutput](
+        sql_executor=sql_executor,
+        top_level_query_handler_context=top_level_query_handler_context,
+        parameter=test_input,
+        query_handler_factory=ContinueQueryListTestQueryHandler
+    )
+    test_output = query_handler_runner.run()
+    assert test_output.test_input == test_input and \
+           sql_executor.queries == [
+               f"""SELECT 1""",
+               f"""CREATE VIEW "{temporary_schema_name}"."temp_db_object_2_1" AS SELECT 1 as "a";""",
+               f"""SELECT "a" FROM "{temporary_schema_name}"."temp_db_object_2_1";""",
+               f"""DROP VIEW IF EXISTS "{temporary_schema_name}"."temp_db_object_2_1";""",
+           ]
+
+
+class ContinueErrorCleanupQueriesTestQueryHandler(QueryHandler[TestInput, TestOutput]):
+    def __init__(self, parameter: TestInput, query_handler_context: ScopeQueryHandlerContext):
+        super().__init__(parameter, query_handler_context)
+        self._parameter = parameter
+
+    def start(self) -> Union[Continue, Finish[TestOutput]]:
+        column_name = ColumnName("a")
+        input_query = SelectQueryWithColumnDefinition(f"""SELECT 1 as {column_name.quoted_name()}""",
+                                                      [Column(column_name, ColumnType("INTEGER"))])
+        return Continue(query_list=[], input_query=input_query)
+
+    def handle_query_result(self, query_result: QueryResult) -> Union[Continue, Finish[TestOutput]]:
+        self._query_handler_context.get_temporary_table()
+        raise Exception("Start failed")
+
+
+def test_continue_error_cleanup_queries(temporary_schema_name, top_level_query_handler_context):
+    input_query_create_view_result_set = MockResultSet()
+    input_query_result_set = MockResultSet(rows=[(1,)], columns={"a": {"type": "INTEGER"}})
+    drop_table_result_set = MockResultSet()
+    drop_input_query_view_result_set = MockResultSet()
+    sql_executor = MockSQLExecutor(
+        result_sets=[
+            input_query_create_view_result_set,
+            input_query_result_set,
+            drop_table_result_set,
+            drop_input_query_view_result_set
+        ])
+    test_input = TestInput()
+    query_handler_runner = MockQueryHandlerRunner[TestInput, TestOutput](
+        sql_executor=sql_executor,
+        top_level_query_handler_context=top_level_query_handler_context,
+        parameter=test_input,
+        query_handler_factory=ContinueErrorCleanupQueriesTestQueryHandler
+    )
+    with pytest.raises(Exception, match="Execution of query handler .* failed."):
+        test_output = query_handler_runner.run()
+    assert sql_executor.queries == [
+        f"""CREATE VIEW "{temporary_schema_name}"."temp_db_object_2_1" AS SELECT 1 as "a";""",
+        f"""SELECT "a" FROM "{temporary_schema_name}"."temp_db_object_2_1";""",
+        f"""DROP TABLE IF EXISTS "{temporary_schema_name}"."temp_db_object_3";""",
+        f"""DROP VIEW IF EXISTS "{temporary_schema_name}"."temp_db_object_2_1";""",
+    ]
+
+
+class ContinueContinueFinishTestQueryHandler(QueryHandler[TestInput, TestOutput]):
+    def __init__(self, parameter: TestInput, query_handler_context: ScopeQueryHandlerContext):
+        super().__init__(parameter, query_handler_context)
+        self._parameter = parameter
+        self._iter = 0
+
+    def start(self) -> Union[Continue, Finish[TestOutput]]:
+        column_name = ColumnName("a")
+        input_query = SelectQueryWithColumnDefinition(f"""SELECT 1 as {column_name.quoted_name()}""",
+                                                      [Column(column_name, ColumnType("INTEGER"))])
+        return Continue(query_list=[], input_query=input_query)
+
+    def handle_query_result(self, query_result: QueryResult) -> Union[Continue, Finish[TestOutput]]:
+        if self._iter == 0:
+            self._iter += 1
+            column_name = ColumnName("b")
+            input_query = SelectQueryWithColumnDefinition(f"""SELECT 1 as {column_name.quoted_name()}""",
+                                                          [Column(column_name, ColumnType("INTEGER"))])
+            return Continue(query_list=[], input_query=input_query)
+        else:
+            return Finish[TestOutput](TestOutput(self._parameter))
+
+
+def test_continue_continue_finish(temporary_schema_name, top_level_query_handler_context):
+    input_query_create_view_result_set = MockResultSet()
+    input_query_result_set1 = MockResultSet(rows=[(1,)], columns={"a": {"type": "INTEGER"}})
+    input_query_result_set2 = MockResultSet(rows=[(1,)], columns={"b": {"type": "INTEGER"}})
+    drop_input_query_view_result_set = MockResultSet()
+    sql_executor = MockSQLExecutor(
+        result_sets=[
+            input_query_create_view_result_set,
+            input_query_result_set1,
+            drop_input_query_view_result_set,
+            input_query_create_view_result_set,
+            input_query_result_set2,
+            drop_input_query_view_result_set,
+        ])
+    temporary_schema_name = "temp_schema_name"
+    test_input = TestInput()
+    query_handler_runner = MockQueryHandlerRunner[TestInput, TestOutput](
+        sql_executor=sql_executor,
+        top_level_query_handler_context=top_level_query_handler_context,
+        parameter=test_input,
+        query_handler_factory=ContinueContinueFinishTestQueryHandler
+    )
+    test_output = query_handler_runner.run()
+    assert test_output.test_input == test_input and \
+           sql_executor.queries == [
+               f"""CREATE VIEW "{temporary_schema_name}"."temp_db_object_2_1" AS SELECT 1 as "a";""",
+               f"""SELECT "a" FROM "{temporary_schema_name}"."temp_db_object_2_1";""",
+               f"""DROP VIEW IF EXISTS "{temporary_schema_name}"."temp_db_object_2_1";""",
+               f"""CREATE VIEW "{temporary_schema_name}"."temp_db_object_4_1" AS SELECT 1 as "b";""",
+               f"""SELECT "b" FROM "{temporary_schema_name}"."temp_db_object_4_1";""",
+               f"""DROP VIEW IF EXISTS "{temporary_schema_name}"."temp_db_object_4_1";""",
+           ]
+
+
+class ContinueContinueCleanupFinishTestQueryHandler(QueryHandler[TestInput, TestOutput]):
+    def __init__(self, parameter: TestInput, query_handler_context: ScopeQueryHandlerContext):
+        super().__init__(parameter, query_handler_context)
+        self._parameter = parameter
+        self._iter = 0
+
+    def start(self) -> Union[Continue, Finish[TestOutput]]:
+        self._child_query_handler_conntext = self._query_handler_context.get_child_query_handler_context()
+        self._table = self._child_query_handler_conntext.get_temporary_table()
+        column_name = ColumnName("a")
+        input_query = SelectQueryWithColumnDefinition(f"""SELECT 1 as {column_name.quoted_name()}""",
+                                                      [Column(column_name, ColumnType("INTEGER"))])
+        return Continue(query_list=[], input_query=input_query)
+
+    def handle_query_result(self, query_result: QueryResult) -> Union[Continue, Finish[TestOutput]]:
+        if self._iter == 0:
+            self._child_query_handler_conntext.release()
+            self._iter += 1
+            column_name = ColumnName("b")
+            input_query = SelectQueryWithColumnDefinition(f"""SELECT 1 as {column_name.quoted_name()}""",
+                                                          [Column(column_name, ColumnType("INTEGER"))])
+            return Continue(query_list=[], input_query=input_query)
+        else:
+            return Finish[TestOutput](TestOutput(self._parameter))
+
+
+def test_continue_cleanup_continue_finish(temporary_schema_name, top_level_query_handler_context):
+    input_query_create_view_result_set = MockResultSet()
+    input_query_result_set1 = MockResultSet(rows=[(1,)], columns={"a": {"type": "INTEGER"}})
+    input_query_result_set2 = MockResultSet(rows=[(1,)], columns={"b": {"type": "INTEGER"}})
+    drop_input_query_view_result_set = MockResultSet()
+    drop_table_result_set = MockResultSet()
+    sql_executor = MockSQLExecutor(
+        result_sets=[
+            input_query_create_view_result_set,
+            input_query_result_set1,
+            drop_input_query_view_result_set,
+            drop_table_result_set,
+            input_query_create_view_result_set,
+            input_query_result_set2,
+            drop_input_query_view_result_set,
+        ])
+    temporary_schema_name = "temp_schema_name"
+    test_input = TestInput()
+    query_handler_runner = MockQueryHandlerRunner[TestInput, TestOutput](
+        sql_executor=sql_executor,
+        top_level_query_handler_context=top_level_query_handler_context,
+        parameter=test_input,
+        query_handler_factory=ContinueContinueCleanupFinishTestQueryHandler
+    )
+    test_output = query_handler_runner.run()
+    assert test_output.test_input == test_input and \
+           sql_executor.queries == [
+               f"""CREATE VIEW "{temporary_schema_name}"."temp_db_object_4_1" AS SELECT 1 as "a";""",
+               f"""SELECT "a" FROM "{temporary_schema_name}"."temp_db_object_4_1";""",
+               f"""DROP VIEW IF EXISTS "{temporary_schema_name}"."temp_db_object_4_1";""",
+               f"""DROP TABLE IF EXISTS "{temporary_schema_name}"."temp_db_object_2_1";""",
+               f"""CREATE VIEW "{temporary_schema_name}"."temp_db_object_6_1" AS SELECT 1 as "b";""",
+               f"""SELECT "b" FROM "{temporary_schema_name}"."temp_db_object_6_1";""",
+               f"""DROP VIEW IF EXISTS "{temporary_schema_name}"."temp_db_object_6_1";""",
+           ]
