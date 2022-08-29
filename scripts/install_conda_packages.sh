3e80b7ffc2b3b61ee602a8d1ed4cbe7592107ace
Torsten Kilias <torsten.kilias@exasol.com>
2022-08-29 08:47:36 +0200

    Removed luarocks tree in favor of using the default tree of the conda installed lua

--- scripts/install_conda_packages.sh
+++ scripts/install_conda_packages.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -i
+set -euo pipefail
+
 micromamba create -y --prefix ./.conda_env -f conda.yml
--- scripts/install_luarocks_packages.sh
+++ scripts/install_luarocks_packages.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/bin/bash -i
 set -euo pipefail
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-$SCRIPT_DIR/run_in_dev_env.sh luarocks --tree .luarocks install advanced-analytics-framework-*.rockspec
+source "$SCRIPT_DIR"/activate_development_environment.sh
+$SCRIPT_DIR/run_in_dev_env.sh luarocks install --only-deps advanced-analytics-framework-*.rockspec
