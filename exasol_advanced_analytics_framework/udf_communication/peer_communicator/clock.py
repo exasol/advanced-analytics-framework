1742f6abab0fe8b7d384be9220845ca258c44e04
Torsten Kilias <tkilias@users.noreply.github.com>
2023-07-04 16:10:59 +0200

    Update exasol_advanced_analytics_framework/udf_communication/peer_communicator/clock.py
    Co-authored-by: Nicola Coretti <nicola.coretti@exasol.com>

--- exasol_advanced_analytics_framework/udf_communication/peer_communicator/clock.py
+++ exasol_advanced_analytics_framework/udf_communication/peer_communicator/clock.py
@@ -2,6 +2,7 @@ import time
 
 
 class Clock():
-    def get_current_timestamp_in_ms(self) -> int:
-        current_timestamp_in_ms = time.monotonic_ns() // 10 ** 6
-        return current_timestamp_in_ms
+    @property
+    def current_timestamp_in_ms(self) -> int:
+        timestamp = time.monotonic_ns() // 10 ** 6
+        return timestamp
