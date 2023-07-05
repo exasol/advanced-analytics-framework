3917de6b8df44626e21edf4bf130698770cb8ecb
Torsten Kilias <torsten.kilias@exasol.com>
2023-07-05 09:10:00 +0200

    Revert changing Clock.get_current_timestamp_in_ms to property, Properties can't be properly tested with mocks.

--- exasol_advanced_analytics_framework/udf_communication/peer_communicator/clock.py
+++ exasol_advanced_analytics_framework/udf_communication/peer_communicator/clock.py
@@ -2,7 +2,6 @@ import time
 
 
 class Clock():
-    @property
-    def current_timestamp_in_ms(self) -> int:
+    def get_current_timestamp_in_ms(self) -> int:
         timestamp = time.monotonic_ns() // 10 ** 6
         return timestamp
