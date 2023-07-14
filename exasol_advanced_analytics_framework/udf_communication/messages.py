942e4e97ab6b6c7312fbe0f330161a278c8f0a1d
Torsten Kilias <torsten.kilias@exasol.com>
2023-07-15 00:38:48 +0200

    Fix message_type in messages.py

--- exasol_advanced_analytics_framework/udf_communication/messages.py
+++ exasol_advanced_analytics_framework/udf_communication/messages.py
@@ -7,59 +7,59 @@ from exasol_advanced_analytics_framework.udf_communication.peer import Peer
 
 
 class RegisterPeer(BaseModel, frozen=True):
-    message_type: Literal["RegisterPeerMessage"] = "RegisterPeerMessage"
+    message_type: Literal["RegisterPeer"] = "RegisterPeer"
     peer: Peer
     source: Optional["Peer"]
 
 
 class AcknowledgeRegisterPeer(BaseModel, frozen=True):
-    message_type: Literal["AcknowledgeRegisterPeerMessage"] = "AcknowledgeRegisterPeerMessage"
+    message_type: Literal["AcknowledgeRegisterPeer"] = "AcknowledgeRegisterPeer"
     peer: Peer
     source: Peer
 
 
 class RegisterPeerComplete(BaseModel, frozen=True):
-    message_type: Literal["RegisterPeerCompleteMessage"] = "RegisterPeerCompleteMessage"
+    message_type: Literal["RegisterPeerComplete"] = "RegisterPeerComplete"
     peer: Peer
     source: Peer
 
 
 class PeerIsReadyToReceive(BaseModel, frozen=True):
-    message_type: Literal["PeerIsReadyToReceiveMessage"] = "PeerIsReadyToReceiveMessage"
+    message_type: Literal["PeerIsReadyToReceive"] = "PeerIsReadyToReceive"
     peer: Peer
 
 
 class Ping(BaseModel, frozen=True):
-    message_type: Literal["PingMessage"] = "PingMessage"
+    message_type: Literal["Ping"] = "Ping"
     source: ConnectionInfo
 
 
 class Stop(BaseModel, frozen=True):
-    message_type: Literal["StopMessage"] = "StopMessage"
+    message_type: Literal["Stop"] = "Stop"
 
 
 class Payload(BaseModel, frozen=True):
-    message_type: Literal["PayloadMessage"] = "PayloadMessage"
+    message_type: Literal["Payload"] = "Payload"
     source: ConnectionInfo
 
 
 class MyConnectionInfo(BaseModel, frozen=True):
-    message_type: Literal["MyConnectionInfoMessage"] = "MyConnectionInfoMessage"
+    message_type: Literal["MyConnectionInfo"] = "MyConnectionInfo"
     my_connection_info: ConnectionInfo
 
 
 class SynchronizeConnection(BaseModel, frozen=True):
-    message_type: Literal["SynchronizeConnectionMessage"] = "SynchronizeConnectionMessage"
+    message_type: Literal["SynchronizeConnection"] = "SynchronizeConnection"
     source: ConnectionInfo
 
 
 class AcknowledgeConnection(BaseModel, frozen=True):
-    message_type: Literal["AcknowledgeConnectionMessage"] = "AcknowledgeConnectionMessage"
+    message_type: Literal["AcknowledgeConnection"] = "AcknowledgeConnection"
     source: ConnectionInfo
 
 
 class Timeout(BaseModel, frozen=True):
-    message_type: Literal["TimeoutMessage"] = "TimeoutMessage"
+    message_type: Literal["Timeout"] = "Timeout"
 
 
 class Message(BaseModel, frozen=True):
