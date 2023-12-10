10209bc1864f80489faba1e38a28149558260e3a
Torsten Kilias <torsten.kilias@exasol.com>
2023-12-10 19:32:06 +0100

    Use extracted exchange_cluster_information in distributed_training_udf.py

--- exasol_advanced_analytics_framework/tensorflow/distributed_training_udf.py
+++ exasol_advanced_analytics_framework/tensorflow/distributed_training_udf.py
@@ -1,50 +1,16 @@
-import contextlib
-import socket
-from typing import Iterator
-from typing import List
-
 import structlog
 import tensorflow as tf
-from pydantic import BaseModel
 from structlog.typing import FilteringBoundLogger
 from tensorflow.python.distribute.cluster_resolver import SimpleClusterResolver, ClusterResolver
 from tensorflow.python.keras import activations
 from tensorflow.python.training.server_lib import ClusterSpec
 
 from exasol_advanced_analytics_framework.udf_communication.distributed_udf import DistributedUDF, \
-    exchange_cluster_information, UDFCommunicatorFactory
-from exasol_advanced_analytics_framework.udf_communication.ip_address import Port, IPAddress
+    UDFCommunicatorFactory
+from exasol_advanced_analytics_framework.udf_communication.exchange_cluster_information import \
+    exchange_cluster_information, reserve_port, WorkerAddress, ClusterInformation
 
 LOGGER: FilteringBoundLogger = structlog.get_logger()
-
-
-class WorkerAddress(BaseModel):
-    ip_address: IPAddress
-    port: Port
-
-
-class ClusterInformation(BaseModel):
-    workers: List[WorkerAddress]
-
-
-@contextlib.contextmanager
-def reserve_port(ip: IPAddress) -> Iterator[Port]:
-    def new_socket():
-        return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    def bind(sock: socket.socket, ip: IPAddress, port: int):
-        sock.bind((ip.ip_address, port))
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    def acquire_port_number(sock: socket.socket, ip: IPAddress) -> int:
-        bind(sock, ip, 0)
-        return sock.getsockname()[1]
-
-    with new_socket() as sock:
-        port_number = acquire_port_number(sock, ip)
-        port = Port(port=port_number)
-        LOGGER.info("reserve_port", ip=ip, port=port)
-        yield port
 
 
 class DistributedTrainingUDF(DistributedUDF):
