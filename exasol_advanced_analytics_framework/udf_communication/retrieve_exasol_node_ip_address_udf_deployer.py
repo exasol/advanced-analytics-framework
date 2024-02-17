dec8e3d739aeaa9f36e3f936c992a2395938f91e
Torsten Kilias <torsten.kilias@exasol.com>
2024-02-17 20:56:32 +0100

    use cleandoc in RetrieveExasolNodeIPAddressUDFDeployer

--- exasol_advanced_analytics_framework/udf_communication/retrieve_exasol_node_ip_address_udf_deployer.py
+++ exasol_advanced_analytics_framework/udf_communication/retrieve_exasol_node_ip_address_udf_deployer.py
@@ -1,4 +1,5 @@
 import textwrap
+from inspect import cleandoc
 
 from exasol_data_science_utils_python.schema.udf_name import UDFName
 
@@ -6,7 +7,7 @@ from exasol_data_science_utils_python.schema.udf_name import UDFName
 class RetrieveExasolNodeIPAddressUDFDeployer:
 
     def render_udf_create_statement(self, udf_name: UDFName, language_alias: str):
-        udf_create_statement = textwrap.dedent(f"""
+        udf_create_statement = cleandoc(f"""
             CREATE OR REPLACE  {language_alias} SET SCRIPT {udf_name.fully_qualified}(ignored INTEGER)
             EMITS (ip_address VARCHAR(10), network_prefix INTEGER) AS
             from exasol_advanced_analytics_framework.udf_communication.retrieve_exasol_node_ip_address_udf import RetrieveExasolNodeIPAddressUDF
