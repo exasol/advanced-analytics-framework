55cffaaa602302dc8fb4d862cff65d68b074e7d5
Torsten Kilias <torsten.kilias@exasol.com>
2024-02-17 18:27:57 +0100

    Add MockSQLExecutor which also checks the query to execute against expected queries

---
+++ exasol_advanced_analytics_framework/udf_utils/testing/mock_sql_executor.py
@@ -0,0 +1,34 @@
+import dataclasses
+from inspect import cleandoc
+from typing import Optional, List, Dict, Tuple
+
+from exasol_data_science_utils_python.udf_utils.sql_executor import SQLExecutor, ResultSet
+from exasol_data_science_utils_python.udf_utils.testing.mock_result_set import MockResultSet
+
+
+@dataclasses.dataclass
+class ExpectedQuey:
+    expected_query: str
+    mock_result_set: MockResultSet
+
+
+class MockSQLExecutor(SQLExecutor):
+    def __init__(self, expected_queries: Optional[List[ExpectedQuey]] = None):
+        self._expected_queries = expected_queries
+        self._expected_query_iterator = iter(expected_queries)
+
+    def execute(self, sql: str) -> ResultSet:
+        if self._expected_queries is None:
+            return MockResultSet()
+        else:
+            try:
+                next_expected_query = next(self._expected_query_iterator)
+                if next_expected_query.expected_query != sql:
+                    raise RuntimeError(
+                        cleandoc(
+                            f"""Expected query
+{next_expected_query.expected_query}
+but got
+{sql}"""))
+            except StopIteration as e:
+                raise RuntimeError(f"No result set found for query {sql}")
