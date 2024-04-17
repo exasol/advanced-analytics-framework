80afdc28788c65f1cfb260bcad01cac8da6210eb
Torsten Kilias <torsten.kilias@exasol.com>
2024-04-17 11:02:40 +0200

    Fix VARCHAR size of ip_adress in RetrieveExasolNodeIPAddressUDFDeployer

--- exasol_advanced_analytics_framework/udf_communication/retrieve_exasol_node_ip_address_udf_deployer.py
+++ exasol_advanced_analytics_framework/udf_communication/retrieve_exasol_node_ip_address_udf_deployer.py
@@ -9,7 +9,7 @@ class RetrieveExasolNodeIPAddressUDFDeployer:
     def render_udf_create_statement(self, udf_name: UDFName, language_alias: str):
         udf_create_statement = cleandoc(f"""
             CREATE OR REPLACE  {language_alias} SET SCRIPT {udf_name.fully_qualified}(ignored INTEGER)
-            EMITS (ip_address VARCHAR(10), network_prefix INTEGER) AS
+            EMITS (ip_address VARCHAR(1000), network_prefix INTEGER) AS
             from exasol_advanced_analytics_framework.udf_communication.retrieve_exasol_node_ip_address_udf import RetrieveExasolNodeIPAddressUDF
     
             def run(ctx):
