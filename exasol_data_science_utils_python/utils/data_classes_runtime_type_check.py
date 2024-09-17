ce76e795f3f2ba0f9c4b1613d20fbff99a2ab27e
ckunki <christoph.kuhnke@exasol.com>
2024-09-17 12:07:59 +0200

    Removed typguard argument argname

--- exasol_data_science_utils_python/utils/data_classes_runtime_type_check.py
+++ exasol_data_science_utils_python/utils/data_classes_runtime_type_check.py
@@ -5,6 +5,7 @@ import typeguard
 
 def check_dataclass_types(datacls):
     for field in fields(datacls):
-        typeguard.check_type(value=datacls.__dict__[field.name],
-                             expected_type=field.type,
-                             argname=field.name)
+        typeguard.check_type(
+            value=datacls.__dict__[field.name],
+            expected_type=field.type,
+        ) # removed argname=field.name
