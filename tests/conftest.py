b9c76218387c422294b78733b272afa552e8b659
ckunki <christoph.kuhnke@exasol.com>
2024-09-17 11:55:01 +0200

    Fixed typeguard error

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 pytest_plugins = [
     "tests.integration_tests.with_db.fixtures.database_connection_fixture",
-    "tests.integration_tests.with_db.fixtures.build_language_container_fixture", # obsolete
-    "tests.integration_tests.with_db.fixtures.upload_language_container_fixture", # obsolete
+    # "tests.integration_tests.with_db.fixtures.build_language_container_fixture", # obsolete
+    # "tests.integration_tests.with_db.fixtures.upload_language_container_fixture", # obsolete
     "tests.integration_tests.with_db.fixtures.slc_fixtures",
     "tests.integration_tests.with_db.fixtures.setup_database_fixture",
     "tests.unit_tests.query_handler.fixtures"
