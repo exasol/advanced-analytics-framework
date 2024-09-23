2d91514bc271f208ff45503e2c4db066ec5752d0
ckunki <christoph.kuhnke@exasol.com>
2024-09-23 09:07:26 +0200

    Experimental fix for PYTSLC

--- tests/integration_tests/with_db/conftest.py
+++ tests/integration_tests/with_db/conftest.py
@@ -8,11 +8,33 @@ from exasol.python_extension_common.deployment.language_container_builder import
 
 
 @pytest.fixture(scope="session")
-def slc_builder() -> LanguageContainerBuilder:
+def slc_builder(use_onprem, use_saas) -> LanguageContainerBuilder:
     """
     Overrides default definition from pytest-exasol-slc.
     """
-    test_package = find_path_backwards("tests/test_package", __file__)
-    with custom_slc_builder() as builder:
-        builder.prepare_flavor(test_package)
-        yield builder
+    if use_saas or use_onprem:
+        test_package = find_path_backwards("tests/test_package", __file__)
+        with custom_slc_builder() as builder:
+            builder.prepare_flavor(test_package)
+            yield builder
+    else:
+        yield None
+
+
+# can be removed as soon as the following issues is fixed and
+# a new version of PYTSLC is released
+# https://github.com/exasol/pytest-plugins/issues/58
+@pytest.fixture(scope="session")
+def upload_slc(slc_builder, export_slc, pyexasol_connection, backend_aware_bucketfs_params):
+    """
+    The fixture uploads language container to a database, according to the selected
+    backends.
+    """
+    if (slc_builder is not None) and (export_slc is not None):
+        # pyexasol_connection = pyexasol.connect(**backend_aware_database_params)
+        bucketfs_path = bfs.path.build_path(**backend_aware_bucketfs_params,
+                                            path=BFS_CONTAINER_DIRECTORY)
+        deployer = LanguageContainerDeployer(pyexasol_connection=pyexasol_connection,
+                                             bucketfs_path=bucketfs_path,
+                                             language_alias=slc_builder.language_alias)
+        deployer.run(container_file=export_slc, alter_system=True, allow_override=True)
