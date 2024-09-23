b03f819ed3267feb19c500f2d38bbd9f66d0f493
ckunki <christoph.kuhnke@exasol.com>
2024-09-23 16:28:25 +0200

    Ensure port is not empty

--- tests/integration_tests/with_db/fixtures/setup_database_fixture.py
+++ tests/integration_tests/with_db/fixtures/setup_database_fixture.py
@@ -1,6 +1,6 @@
 import pytest
 import pyexasol
-from typing import Tuple
+from typing import Any, Tuple
 from exasol_advanced_analytics_framework.deployment.scripts_deployer import ScriptsDeployer
 
 
@@ -54,18 +54,20 @@ def _deploy_scripts(db_conn) -> None:
 # }
 
 
-def _bucket_address(bucketfs_params) -> str:
+import re
+def _bucket_address(bucketfs_params: dict[str, Any]) -> str:
     # pytest-plugins/pytest-slc/exasol/pytest_slc/__init__.py defines
     BFS_CONTAINER_DIRECTORY = 'container'
     host_and_port = bucketfs_params["url"]
+    port = ":6666" if not re.search(":[0-9]+$", host_and_port) else ""
     bucket_name = bucketfs_params["bucket_name"]
     path_in_bucket = BFS_CONTAINER_DIRECTORY
     service_name = bucketfs_params["service_name"]
-    return ( f"http://{host_and_port}/{bucket_name}/"
+    return ( f"http://{host_and_port}{port}/{bucket_name}/"
              f"{path_in_bucket};{service_name}" )
 
 
-def _create_bucketfs_connection(use_onprem, db_conn, bucketfs_params) -> str:
+def _create_bucketfs_connection(use_onprem, db_conn, bucketfs_params: dict[str, Any]) -> str:
     name = BUCKETFS_CONNECTION_NAME
     if use_onprem:
         # In general currently I disabled SaaS backend for AAF.
