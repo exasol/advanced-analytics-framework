15dd59ad21f5db29323f6485c37819c9c551beca
ckunki <christoph.kuhnke@exasol.com>
2024-09-24 15:04:00 +0200

    Removed remaining references to old slc_deployer implementation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,4 @@
-# the commented out plugins are obsolete after removing
-# tests/deployment/test_scripts_deployer.py and
-# tests/deployment/test_scripts_deployer_cli.py
-
 pytest_plugins = [
-    # "tests.integration_tests.with_db.fixtures.build_language_container_fixture",
-    # "tests.integration_tests.with_db.fixtures.upload_language_container_fixture",
     "tests.integration_tests.with_db.fixtures.setup_database_fixture",
     "tests.unit_tests.query_handler.fixtures"
 ]
--- tests/utils/revert_language_settings.py
+++ tests/utils/revert_language_settings.py
@@ -1,33 +1,5 @@
 import contextlib
 import pyexasol   # type: ignore
-import pytest
-from tests.utils.parameters import db_params
-
-# obsolete, only used by
-# tests/deployment/test__deployer_cli.py
-# tests/deployment/test__deployer.py
-def revert_language_settings_old(func):
-    def wrapper(language_alias, schema, db_conn,
-                container_path, language_settings):
-        try:
-            return func(language_alias, schema, db_conn,
-                        container_path, language_settings)
-        except Exception as exc:
-            print("Exception occurred while running the test: %s" % exc)
-            raise pytest.fail(exc)
-        finally:
-            print("Revert language settings")
-            db_conn_revert = pyexasol.connect(
-                dsn=db_params.address(),
-                user=db_params.user,
-                password=db_params.password)
-            db_conn_revert.execute(f"ALTER SYSTEM SET SCRIPT_LANGUAGES="
-                                   f"'{language_settings[0][0]}';")
-            db_conn_revert.execute(f"ALTER SESSION SET SCRIPT_LANGUAGES="
-                                   f"'{language_settings[0][1]}';")
-
-    return wrapper
-
 
 @contextlib.contextmanager
 def revert_language_settings(connection: pyexasol.ExaConnection):
