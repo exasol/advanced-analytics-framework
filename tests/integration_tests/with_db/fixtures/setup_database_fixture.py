d93646f7b670a94e87a0776316e8fe31a97a98b4
ckunki <christoph.kuhnke@exasol.com>
2024-09-24 13:39:11 +0200

    as fixture

--- tests/integration_tests/with_db/fixtures/setup_database_fixture.py
+++ tests/integration_tests/with_db/fixtures/setup_database_fixture.py
@@ -1,6 +1,6 @@
 import pytest
 import pyexasol
-from typing import Any, Tuple
+from typing import Any, Tuple, Callable
 from exasol_advanced_analytics_framework.deployment.scripts_deployer import ScriptsDeployer
 from exasol_advanced_analytics_framework.slc import LANGUAGE_ALIAS
 
@@ -28,22 +28,31 @@ def _deploy_scripts(db_conn) -> None:
         develop=True)
 
 
-def _bucket_address(bucketfs_params: dict[str, Any]) -> str:
+def _bucket_address(
+        bucketfs_params: dict[str, Any],
+        path_in_bucket: str = "my-folder",
+) -> str:
     # pytest-plugins/pytest-slc/exasol/pytest_slc/__init__.py defines
     # BFS_CONTAINER_DIRECTORY = 'container'
     url = bucketfs_params["url"]
     bucket_name = bucketfs_params["bucket_name"]
     # path_in_bucket = BFS_CONTAINER_DIRECTORY
-    path_in_bucket = "my-folder"
+    # path_in_bucket = "my-folder"
     service_name = bucketfs_params["service_name"]
     return ( f"{url}/{bucket_name}/"
              f"{path_in_bucket};{service_name}" )
 
 
-def _create_bucketfs_connection(use_onprem, db_conn, bucketfs_params: dict[str, Any]) -> str:
-    name = BUCKETFS_CONNECTION_NAME
+def _create_bucketfs_connection(
+        use_onprem,
+        db_conn,
+        bucketfs_params: dict[str, Any],
+        name: str = BUCKETFS_CONNECTION_NAME,
+        path_in_bucket: str = "my-folder",
+) -> str:
+    name = name
     if use_onprem:
-        uri = _bucket_address(bucketfs_params)
+        uri = _bucket_address(bucketfs_params, path_in_bucket)
         user = bucketfs_params["username"]
         pwd = bucketfs_params["password"]
         db_conn.execute(
@@ -53,21 +62,45 @@ def _create_bucketfs_connection(use_onprem, db_conn, bucketfs_params: dict[str, 
     return name
 
 
+@pytest.fixture
+def my_bucketfs_connection_factory(
+        use_onprem,
+        db_conn,
+        backend_aware_bucketfs_params,
+) -> Callable[[str, str|None], None]:
+    def create(name, path_in_bucket):
+        _create_bucketfs_connection(
+            use_onprem,
+            db_conn,
+            backend_aware_bucketfs_params,
+            name,
+            path_in_bucket,
+        )
+    return create
+
+
 @pytest.fixture(scope="module")
 def database_with_slc(
         use_onprem,
         pyexasol_connection,
         backend_aware_bucketfs_params,
-        # bucketfs_connection_factory,
+        bucketfs_connection_factory,
+        my_bucketfs_connection_factory,
         upload_slc,
 ) -> Tuple[str|None, str]:
     schema = _create_schema(pyexasol_connection)
     _deploy_scripts(pyexasol_connection)
     # this requires updating query_handler_runner_udf.py to the new bucketfs API, first:
     # bucketfs_connection_factory(BUCKETFS_CONNECTION_NAME, "my-folder")
-    bfs_conn = _create_bucketfs_connection(
-        use_onprem,
-        pyexasol_connection,
-        backend_aware_bucketfs_params,
-    )
+    # my_factory = my_bucketfs_connection_factory(
+    #     use_onprem,
+    #     pyexasol_connection,
+    #     backend_aware_bucketfs_params,
+    # )
+    my_bucketfs_connection_factory(BUCKETFS_CONNECTION_NAME, "my-folder")
+    # bfs_conn = _create_bucketfs_connection(
+    #     use_onprem,
+    #     pyexasol_connection,
+    #     backend_aware_bucketfs_params,
+    # )
     return BUCKETFS_CONNECTION_NAME, schema
