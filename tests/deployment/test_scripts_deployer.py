b1fac53b197b3bbe4b19735d6ae3b3871f25a21c
ckunki <christoph.kuhnke@exasol.com>
2024-09-24 16:34:17 +0200

    Cleanup

--- tests/deployment/test_scripts_deployer.py
+++ tests/deployment/test_scripts_deployer.py
@@ -4,21 +4,16 @@ from tests.utils.db_queries import DBQueries
 from tests.utils.parameters import db_params
 
 
-@pytest.mark.skip(reason="No need to test deployer provided by PEC.")
-def test_scripts_deployer(upload_language_container,
-                          pyexasol_connection, request):
-
+def test_scripts_deployer(deployed_slc, language_alias, pyexasol_connection, request):
     schema_name = request.node.name
     pyexasol_connection.execute(f"DROP SCHEMA IF EXISTS {schema_name} CASCADE;")
-
-    language_alias = upload_language_container
-    ScriptsDeployer.run(
-        dsn=db_params.address(),
-        user=db_params.user,
-        password=db_params.password,
+    ScriptsDeployer.run2(
+        pyexasol_connection
+        # dsn=db_params.address(),
+        # user=db_params.user,
+        # password=db_params.password,
         schema=schema_name,
         language_alias=language_alias,
-        develop=True
-    )
+        develop=True)
     assert DBQueries.check_all_scripts_deployed(
         pyexasol_connection, schema_name)
