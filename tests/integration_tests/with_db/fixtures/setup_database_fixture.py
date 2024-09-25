a30ab7ae3d0cf6321cccddbefdd160363900002e
ckunki <christoph.kuhnke@exasol.com>
2024-09-25 16:48:09 +0200

    Cleanup

--- tests/integration_tests/with_db/fixtures/setup_database_fixture.py
+++ tests/integration_tests/with_db/fixtures/setup_database_fixture.py
@@ -5,7 +5,6 @@ from exasol_advanced_analytics_framework.deployment.scripts_deployer import Scri
 
 
 BUCKETFS_CONNECTION_NAME = "TEST_AAF_BFS_CONN"
-# SCHEMA_NAME = "TEST_INTEGRATION"
 
 
 @pytest.fixture(scope="session")
@@ -14,26 +13,6 @@ def db_schema_name() -> str:
     Overrides default fixture from pytest-exasol-extension.
     """
     return "TEST_INTEGRATION"
-
-
-# @pytest.fixture(scope="session")
-# def pyexasol_connection(backend_aware_database_params) -> pyexasol.ExaConnection:
-#     return pyexasol.connect(**backend_aware_database_params)
-
-# obsolete
-def _create_schema(db_conn) -> str:
-    schema = SCHEMA_NAME
-    db_conn.execute(f"DROP SCHEMA IF EXISTS {schema} CASCADE;")
-    db_conn.execute(f"CREATE SCHEMA IF NOT EXISTS {schema};")
-    return schema
-
-# obsolete
-def _deploy_scripts(db_conn, schema_name, language_alias) -> None:
-    ScriptsDeployer.run(
-        db_conn,
-        schema=schema_name,
-        language_alias=language_alias,
-        develop=True)
 
 
 @pytest.fixture(scope="module")
@@ -58,26 +37,6 @@ def _bucket_address(
              f"{path_in_bucket};{service_name}" )
 
 
-# # Can be removed after
-# # https://github.com/exasol/advanced-analytics-framework/issues/176
-# def _create_bucketfs_connection(
-#         use_onprem,
-#         db_conn,
-#         bucketfs_params: dict[str, Any],
-#         name: str = BUCKETFS_CONNECTION_NAME,
-#         path_in_bucket: str = "my-folder",
-# ) -> str:
-#     if use_onprem:
-#         uri = _bucket_address(bucketfs_params, path_in_bucket)
-#         user = bucketfs_params["username"]
-#         pwd = bucketfs_params["password"]
-#         db_conn.execute(
-#             f"CREATE OR REPLACE  CONNECTION {name} TO '{uri}' " \
-#             f"USER '{user}' IDENTIFIED BY '{pwd}'"
-#         )
-#     return name
-
-
 # Can be removed after
 # https://github.com/exasol/advanced-analytics-framework/issues/176
 @pytest.fixture(scope='session')
@@ -97,13 +56,6 @@ def my_bucketfs_connection_factory(
             f"CREATE OR REPLACE  CONNECTION {name} TO '{uri}' " \
             f"USER '{user}' IDENTIFIED BY '{pwd}'"
         )
-        # _create_bucketfs_connection(
-        #     use_onprem,
-        #     pyexasol_connection,
-        #     backend_aware_bucketfs_params,
-        #     name,
-        #     path_in_bucket,
-        # )
     return create
 
 
@@ -116,12 +68,6 @@ def database_with_slc(
         my_bucketfs_connection_factory,
         deployed_slc,
 ) -> Tuple[str|None, str]:
-    # Done by pyexasol_connection from pytest-exasol-extension:
-    # schema = _create_schema(pyexasol_connection)
-
-    # replaced by fixture
-    # _deploy_scripts(pyexasol_connection, language_alias, db_schema_name)
-
     # this requires updating query_handler_runner_udf.py to the new bucketfs API, first,
     # which is planned to be done in ticket
     # https://github.com/exasol/advanced-analytics-framework/issues/176
