0242e26b8b350dff0edb47d87403d1bb1d11d05a
ckunki <christoph.kuhnke@exasol.com>
2024-09-26 11:10:10 +0200

    pinned micromamba version to 1.5.10
    latest version 2.0 contains breaking changes.
    Additionally maybe we should obtain micromamba from github?

--- scripts/install_micromamba.sh
+++ scripts/install_micromamba.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 cd $HOME
 mkdir bin || true
-wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+# wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/1.5.10 | tar -xvj bin/micromamba
 ./bin/micromamba shell init -s bash -p ~/micromamba
 
 echo 'export PATH="$HOME/bin:$PATH"' >>"$HOME"/.bashrc
