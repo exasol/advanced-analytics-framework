8c5c6d3fd72e795628f1733feb2ab215a3f9e167
ckunki <christoph.kuhnke@exasol.com>
2024-09-26 13:49:45 +0200

    #184 Updated micromamba to version 2.0.0

--- scripts/install_micromamba.sh
+++ scripts/install_micromamba.sh
@@ -5,7 +5,8 @@ cd $HOME
 mkdir bin || true
 # See https://github.com/exasol/advanced-analytics-framework/issues/184
 # wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
-wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/1.5.10 | tar -xvj bin/micromamba
+# wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/1.5.10 | tar -xvj bin/micromamba
+wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/2.0.0 | tar -xvj bin/micromamba
 ./bin/micromamba shell init -s bash --root-prefix ~/micromamba
 
 echo 'export PATH="$HOME/bin:$PATH"' >>"$HOME"/.bashrc
