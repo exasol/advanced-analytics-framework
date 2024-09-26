d8e868c6286cf2da9a4a224d44c96b9a06e2b001
ckunki <christoph.kuhnke@exasol.com>
2024-09-26 11:59:10 +0200

    Fixed call to micromamba

--- scripts/install_micromamba.sh
+++ scripts/install_micromamba.sh
@@ -6,7 +6,7 @@ mkdir bin || true
 # See https://github.com/exasol/advanced-analytics-framework/issues/184
 # wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
 wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/1.5.10 | tar -xvj bin/micromamba
-./bin/micromamba shell init -s bash -p ~/micromamba
+./bin/micromamba shell init -s bash --root-prefix ~/micromamba
 
 echo 'export PATH="$HOME/bin:$PATH"' >>"$HOME"/.bashrc
 export PATH="$HOME/bin:$PATH"
