b4ee260228173f66eeaa5e570139e20e1ef87201
ckunki <christoph.kuhnke@exasol.com>
2024-10-09 14:04:27 +0200

    Re-amalgated lua script

--- exasol_advanced_analytics_framework/resources/outputs/create_query_loop.sql
+++ exasol_advanced_analytics_framework/resources/outputs/create_query_loop.sql
@@ -666,8 +666,8 @@ query_handler_runner = require("query_handler_runner")
 ---
 -- This is the main function of the Query Loop.
 --
--- @param json_str	input parameters as JSON string
--- @param exa	the database context (`exa`) of the Lua script
+-- @param json_str input parameters as JSON string
+-- @param exa	   the database context (`exa`) of the Lua script
 --
 function query_handler_runner_main(json_str, exa)
     return query_handler_runner.run(json_str, exa)
