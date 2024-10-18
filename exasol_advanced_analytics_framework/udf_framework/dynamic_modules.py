9d05c4cf4fd8cd3392cf75773397dae1d188c4b3
ckunki <christoph.kuhnke@exasol.com>
2024-10-18 16:10:36 +0200

    Updated dynamic_modules, preferring functions over a class

--- exasol_advanced_analytics_framework/udf_framework/dynamic_modules.py
+++ exasol_advanced_analytics_framework/udf_framework/dynamic_modules.py
@@ -1,19 +1,24 @@
+import sys
 import importlib
-from typing import Any, List
+from typing import Any
+from types import ModuleType
 
 
-def _new_module(mod_name):
-    spec = importlib.machinery.ModuleSpec(mod_name, None)
-    return importlib.util.module_from_spec(spec)
-
-
-def create_module(name: str, objects: List[Any]):
+def create_module(name: str) -> ModuleType:
     """
-    Dynamically create a python module using the specified name and add
-    the specified objects to the new module. Each object may be a class,
-    variable, or function.
+    Dynamically create a python module using the specified name and
+    register the module in sys.modules[].
+
+    Additionally add a function add_to_module() to the module enabling other
+    code to add classes and functions to the module.
     """
-    mod = _new_module(name)
-    for obj in objects:
-        setattr(mod, obj.__name__, obj)
+    spec = importlib.machinery.ModuleSpec(name, None)
+    mod = importlib.util.module_from_spec(spec)
+    sys.modules[name] = mod
+
+    def add_to_module(object: Any):
+        object.__module__ = name
+        setattr(mod, object.__name__, object)
+
+    add_to_module(add_to_module)
     return mod
--- tests/unit_tests/udf_framework/test_dynamic_modules.py
+++ tests/unit_tests/udf_framework/test_dynamic_modules.py
@@ -1,10 +1,26 @@
 from exasol_advanced_analytics_framework.udf_framework.dynamic_modules import create_module
 
+
 class ExampleClass:
     pass
 
 
-def test_create_module():
-    new_module = create_module("xyz", [ExampleClass])
-    instance = new_module.ExampleClass()
-    assert isinstance(instance, ExampleClass)
+def example_function():
+    return "example_function return value"
+
+
+def test_create_module_with_class():
+    mod = create_module("xx1")
+    mod.add_to_module(ExampleClass)
+    import xx1
+    instance = xx1.ExampleClass()
+    assert isinstance(instance, ExampleClass) and \
+        ExampleClass.__module__ == "xx1"
+
+
+def test_add_function():
+    mod = create_module("xx2")
+    mod.add_to_module(example_function)
+    import xx2
+    assert xx2.example_function() == "example_function return value" \
+        and example_function.__module__ == "xx2"
