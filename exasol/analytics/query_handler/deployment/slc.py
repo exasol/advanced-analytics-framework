7e9e06ce078a9d0d7ccf5d8515186ad0b5da5a42
Torsten Kilias <torsten.kilias@exasol.com>
2025-02-14 16:04:40 +0100

    Add comment to the workaround in slc.py

--- exasol/analytics/query_handler/deployment/slc.py
+++ exasol/analytics/query_handler/deployment/slc.py
@@ -12,16 +12,23 @@ LANGUAGE_ALIAS = "PYTHON3_AAF"
 SLC_NAME = "exasol_advanced_analytics_framework_container"
 SLC_FILE_NAME = SLC_NAME + "_release.tar.gz"
 SLC_URL_FORMATTER = (
-    "https://github.com/exasol/advanced-analytics-framework/releases/download/{version}/"
-    + SLC_FILE_NAME
+        "https://github.com/exasol/advanced-analytics-framework/releases/download/{version}/"
+        + SLC_FILE_NAME
 )
 
 
 class AAFLanguageContainerBuilder(LanguageContainerBuilder):
+    """
+    This subclass is a workaround for problems with poetry export during the SLC build in this project.
+    We investigate the issue further in: https://github.com/exasol/advanced-analytics-framework/issues/253
+    We disabled the cwd=str(project_directory) in the subprocess call and this helps.
+    However, this can cause issues, if you are not in the project directory while building the SLC.
+    """
+
     def _add_requirements_to_flavor(
-        self,
-        project_directory: str | Path,
-        requirement_filter: Callable[[str], bool] | None,
+            self,
+            project_directory: str | Path,
+            requirement_filter: Callable[[str], bool] | None,
     ):
         """
         Adds project's requirements to the requirements.txt file. Creates this file
@@ -29,7 +36,8 @@ class AAFLanguageContainerBuilder(LanguageContainerBuilder):
         """
         assert self._root_path is not None
         requirements_bytes = subprocess.check_output(
-            ["poetry", "export", "--without-hashes", "--without-urls"]
+            ["poetry", "export", "--without-hashes", "--without-urls"],
+            # cwd=str(project_directory) we got the export by disabling this line
         )
         requirements = requirements_bytes.decode("UTF-8")
         if requirement_filter is not None:
