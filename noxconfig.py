32803dea44627b64c8aded1d17df8acd363543be
Torsten Kilias <torsten.kilias@exasol.com>
2025-02-14 10:15:29 +0100

    Ignore .poetry directory which is created for the poetry plugins

--- noxconfig.py
+++ noxconfig.py
@@ -10,7 +10,7 @@ class Config:
     root: Path = ROOT_DIR
     doc: Path = ROOT_DIR / "doc"
     version_file: Path = ROOT_DIR / "version.py"
-    path_filters: Iterable[str] = ("dist", ".eggs", "venv", ".conda_env")
+    path_filters: Iterable[str] = ("dist", ".eggs", "venv", ".conda_env", ".poetry")
 
 
 PROJECT_CONFIG = Config()
