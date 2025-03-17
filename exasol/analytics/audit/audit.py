1019360a7897ffcb58bfd1fafbe8f1d083470814
ckunki <christoph.kuhnke@exasol.com>
2025-03-17 15:42:29 +0100

    removed dead code

--- exasol/analytics/audit/audit.py
+++ exasol/analytics/audit/audit.py
@@ -8,32 +8,6 @@ from exasol.analytics.schema import (
     TableNameImpl,
 )
 
-# class TableDescription:
-#     """
-#     This class describes an SQL table by its attributes
-#     * table (instance of (TableLikeName)) defining the name and optionally the
-#       database schema of the SQL table
-#     * columns the names and types of the columns of the SQL table
-#
-#     The class also offers a property `render_create` which renders the
-#     attributes into an SQL CREATE TABLE statement.
-#     """
-#
-#     def __init__(self, table: TableLikeName, columns: List[Column]):
-#         self.table = table
-#         self.columns = {c.name.name: c for c in columns}
-#
-#     @property
-#     def render_create(self):
-#         columns = ",\n  ".join(c.for_create for c in self.columns.values())
-#         return cleandoc(
-#             f"""
-#             CREATE TABLE IF NOT EXISTS {self.table.fully_qualified} (
-#               {columns}
-#             )
-#             """
-#         )
-
 
 class AuditTable(Table):
     def __init__(
