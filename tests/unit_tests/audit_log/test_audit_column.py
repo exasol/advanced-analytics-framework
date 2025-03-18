407fdb281635c6c01026df094a0b151a2d84cfbf
ckunki <christoph.kuhnke@exasol.com>
2025-03-18 17:46:06 +0100

    Removed obsolete test case

--- tests/unit_tests/audit_log/test_audit_column.py
+++ tests/unit_tests/audit_log/test_audit_column.py
@@ -2,12 +2,6 @@ from exasol.analytics.audit.audit import BaseAuditColumns
 from exasol.analytics.schema import Column
 
 
-def test_docstring():
-    attributes = dir(BaseAuditColumns)
-    assert "ROW_COUNT" in attributes
-    assert "OBJECT_NAME" in attributes
-
-
 def test_access_by_name():
     assert getattr(BaseAuditColumns, "ERROR_MESSAGE") == BaseAuditColumns.ERROR_MESSAGE
 
