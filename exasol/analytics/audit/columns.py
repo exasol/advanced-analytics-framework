bb75433b239796eb3ae94aed0ab08356ddabac1d
ckunki <christoph.kuhnke@exasol.com>
2025-03-18 12:23:37 +0100

    Updated comments for audit columns

--- exasol/analytics/audit/columns.py
+++ exasol/analytics/audit/columns.py
@@ -13,18 +13,21 @@ class BaseAuditColumns:
     # task is to get the RUN_ID.
     # Proposed value: POSIX_TIME(SYSTIMESTAMP(9)) * 1000
     RUN_ID = decimal_column("RUN_ID", precision=20)
+
     ROW_COUNT = decimal_column("ROW_COUNT", precision=36)
-    # For ModifyQuery EVENT_NAME will be set to the Operation Type, optionally
-    # prefixed by "Before " or "After ". Sample values
-    # "Before CREATE_TABLE", "After CREATE_TABLE",
-    # "Before INSERT", "After INSERT",
-    # ERROR, COMMIT, CUSTOM:
-    EVENT_NAME = varchar_column("EVENT_NAME", size=128)
     # SPAN_NAME and SPAN_ID need to be generated and provided by the creator
     # of the AuditQuery, i.e. lower level query_handlers.
+
+    # For ModifyQuery SPAN_NAME will be set to the Operation Type, e.g.
+    # CREATE_TABLE, CREATE_TABLE, INSERT. For other queries it can be a custom
+    # string indicating a specific execution phase.
     SPAN_NAME = varchar_column("LOG_SPAN_NAME", size=2000000)
     SPAN_ID = decimal_column("LOG_SPAN_ID", precision=32)
     PARENT_SPAN_ID = decimal_column("PARENT_LOG_SPAN_ID", precision=32)
+    # For ModifyQuery EVENT_NAME will be either "Begin" or "End".  For other
+    # queries this can be a custom string, e.g.  "ERROR", "COMMIT", ...
+    EVENT_NAME = varchar_column("EVENT_NAME", size=128)
+    # This will contain the string representation of a json document.
     EVENT_ATTRIBUTES = varchar_column("EVENT_ATTRIBUTES", size=2000000)
     OBJECT_TYPE = varchar_column("DB_OBJECT_TYPE", size=128)
     # Optional, can be NULL:
