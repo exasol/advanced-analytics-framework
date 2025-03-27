8b0e38c2f11302b3311163ce9c3d6696c16538d5
ckunki <christoph.kuhnke@exasol.com>
2025-03-27 10:42:22 +0100

    Added test for start method with immediate finish

--- tests/unit_tests/audit_log/test_audit_query_handler.py
+++ tests/unit_tests/audit_log/test_audit_query_handler.py
@@ -5,7 +5,10 @@ from unittest.mock import Mock
 from exasol.analytics.audit.audit_query_handler import (
     AuditQueryHandler,
     ParameterType,
+    ResultType,
 )
+from exasol.analytics.query_handler.query_handler import QueryHandler
+from exasol.analytics.query_handler.result import Finish
 from exasol.analytics.schema import (
     Column,
     decimal_column,
@@ -18,25 +21,38 @@ class MyParameterType(Generic[ParameterType]):
     table_name_prefix: str
 
 
-def test_constructor() -> None:
+def create_audit_query_handler(child: QueryHandler[ParameterType, ResultType]) -> AuditQueryHandler:
     def schema_getter(parameter: MyParameterType) -> str:
         return parameter.db_schema
 
-    def table_prefix_getter(parameter: MyParameterType) -> str:
+    def table_name_prefix_getter(parameter: MyParameterType) -> str:
         return parameter.table_name_prefix
 
-    def additional_columns_getter(parameter: MyParameterType) -> list[Column]:
+    def additional_columns_provider(parameter: MyParameterType) -> list[Column]:
         return [decimal_column("DDD", precision=9)]
 
     parameter = MyParameterType(db_schema="SSS", table_name_prefix="PPP")
-    testee = AuditQueryHandler(
-        parameter,
-        Mock(),
-        Mock(),
-        schema_getter,
-        table_prefix_getter,
-        additional_columns_getter,
+    return AuditQueryHandler(
+        parameter=parameter,
+        context=Mock(),
+        query_handler_factory=Mock(return_value=child),
+        schema_getter=schema_getter,
+        table_name_prefix_getter=table_name_prefix_getter,
+        additional_columns_provider=additional_columns_provider,
     )
+
+
+def test_constructor() -> None:
+    child = Mock()
+    testee = create_audit_query_handler(child)
     audit_table = testee._audit_table
     assert audit_table.name.fully_qualified.startswith('"SSS"."PPP')
     assert '"DDD" DECIMAL(9,0)' in [ c.for_create for c in audit_table.columns ]
+    assert child == testee._child.query_handler
+
+
+def test_start_finish():
+    start_method = Mock(return_value=Finish(result="some result"))
+    child = Mock(start=start_method)
+    actual = create_audit_query_handler(child).start()
+    assert actual.result == "some result"
