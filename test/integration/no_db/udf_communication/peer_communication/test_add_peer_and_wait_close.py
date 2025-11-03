e5a7466c9df3c2f65efa4101e23bfe6d5d858518
ckunki <christoph.kuhnke@exasol.com>
2025-11-03 09:47:15 +0100

    Finished refactoring test_add_peer_and_wait_close.py

--- test/integration/no_db/udf_communication/peer_communication/test_add_peer_and_wait_close.py
+++ test/integration/no_db/udf_communication/peer_communication/test_add_peer_and_wait_close.py
@@ -1,6 +1,11 @@
 import sys
 import time
 import traceback
+from test.integration.no_db.peer_com_runner import (
+    PeerCommunicatorFactory,
+    RepetitionRunner,
+    expect_success,
+)
 from pathlib import Path
 from test.integration.no_db.structlog.structlog_utils import configure_structlog
 from test.integration.no_db.udf_communication.peer_communication.conditional_method_dropper import (
@@ -38,8 +43,6 @@ from exasol.analytics.udf.communication.socket_factory.zmq_wrapper import (
 
 configure_structlog(__file__)
 
-LOGGER: FilteringBoundLogger = structlog.get_logger()
-
 
 def executor(
     logger: FilteringBoundLogger,
@@ -71,101 +74,13 @@ def executor(
         queue.put("Failed")
 
 
-def run(parameter: PeerCommunicatorTestProcessParameter, queue: BidirectionalQueue):
-    logger = LOGGER.bind(
-        group_identifier=parameter.group_identifier, name=parameter.instance_name
-    )
-    try:
-        listen_ip = IPAddress(ip_address=f"127.1.0.1")
-        context = zmq.Context()
-        socket_factory = ZMQSocketFactory(context)
-        com = PeerCommunicator(
-            name=parameter.instance_name,
-            number_of_peers=parameter.number_of_instances,
-            listen_ip=listen_ip,
-            group_identifier=parameter.group_identifier,
-            socket_factory=socket_factory,
-            config=PeerCommunicatorConfig(
-                forward_register_peer_config=ForwardRegisterPeerConfig(
-                    is_leader=False, is_enabled=False
-                )
-            ),
-        )
-        try:
-            queue.put(com.my_connection_info)
-            peer_connection_infos = queue.get()
-            for index, connection_info in peer_connection_infos.items():
-                com.register_peer(connection_info)
-            time.sleep(150)
-        finally:
-            try:
-                com.stop()
-                queue.put("Success")
-            except Exception as e:
-                logger.exception("Exception during test")
-                queue.put("Failed")
-            context.destroy(linger=0)
-            for frame in sys._current_frames().values():
-                stacktrace = traceback.format_stack(frame)
-                logger.info("Frame", stacktrace=stacktrace)
-    except Exception as e:
-        logger.exception("Exception during test")
-        queue.put("Failed")
+RUNNER = RepetitionRunner(
+    __name__,
+    communicator_factory=PeerCommunicatorFactory(),
+    executor=executor,
+    expectation_generator=expect_success,
+)
 
 
-REPETITIONS_FOR_FUNCTIONALITY = 1
-
-
-def test_functionality_2():
-    run_test_with_repetitions(2, REPETITIONS_FOR_FUNCTIONALITY)
-
-
-def run_test_with_repetitions(number_of_instances: int, repetitions: int):
-    for i in range(repetitions):
-        LOGGER.info(
-            f"Start iteration",
-            iteration=i + 1,
-            repetitions=repetitions,
-            number_of_instances=number_of_instances,
-        )
-        start_time = time.monotonic()
-        group = f"{time.monotonic_ns()}"
-        expected_peers_of_threads, peers_of_threads = run_test(
-            group, number_of_instances, seed=i
-        )
-        assert expected_peers_of_threads == peers_of_threads
-        end_time = time.monotonic()
-        LOGGER.info(
-            f"Finish iteration",
-            iteration=i + 1,
-            repetitions=repetitions,
-            number_of_instances=number_of_instances,
-            duration=end_time - start_time,
-        )
-
-
-def run_test(group: str, number_of_instances: int, seed: int):
-    connection_infos: dict[int, ConnectionInfo] = {}
-    parameters = [
-        PeerCommunicatorTestProcessParameter(
-            instance_name=f"i{i}",
-            group_identifier=group,
-            number_of_instances=number_of_instances,
-            seed=seed + i,
-        )
-        for i in range(number_of_instances)
-    ]
-    processes: list[TestProcess[PeerCommunicatorTestProcessParameter]] = [
-        TestProcess(parameter, run=run) for parameter in parameters
-    ]
-    for i in range(number_of_instances):
-        processes[i].start()
-        connection_infos[i] = processes[i].get()
-    for i in range(number_of_instances):
-        t = processes[i].put(connection_infos)
-    assert_processes_finish(processes, timeout_in_seconds=180)
-    result_of_threads: dict[int, list[ConnectionInfo]] = {}
-    for i in range(number_of_instances):
-        result_of_threads[i] = processes[i].get()
-    expected_results_of_threads = {i: "Success" for i in range(number_of_instances)}
-    return expected_results_of_threads, result_of_threads
+def test_functionality():
+    RUNNER.run_multiple(number_of_instances=2, repetitions=1)
