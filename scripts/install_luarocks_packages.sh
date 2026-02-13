e4cab8958999030ed248990fcdc932935fefb183
Torsten Kilias <torsten.kilias@exasol.com>
2026-02-13 09:39:33 +0100

    Pin luarocks to lua54 and various other small fixes

--- scripts/install_luarocks_packages.sh
+++ scripts/install_luarocks_packages.sh
@@ -2,5 +2,4 @@
 set -euo pipefail
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-source "$SCRIPT_DIR"/activate_development_environment.sh
 $SCRIPT_DIR/run_in_dev_env.sh luarocks install --only-deps advanced-analytics-framework-*.rockspec
